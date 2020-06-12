3c664ff880ddc503b3fd7e67f694047e6e6b332b
galina <galina.malov@gmail.com>
2020-06-11 17:16:43 -0700

    mypy fixes

--- kaggle_melanoma/dataloader.py
+++ kaggle_melanoma/dataloader.py
@@ -1,8 +1,8 @@
-import random
 from pathlib import Path
 from typing import List, Tuple, Dict, Any, Union
 
 import albumentations as albu
+import numpy as np
 import torch
 from iglovikov_helper_functions.utils.image_utils import load_rgb
 from pytorch_toolbelt.utils.torch_utils import tensor_from_rgb_image
@@ -10,7 +10,7 @@ from torch.utils.data import Dataset
 
 
 class MelanomaDataset(Dataset):
-    def __init__(self, samples: List[Tuple[Union[Path, str], int]], transform: albu.Compose) -> None:
+    def __init__(self, samples: List[Tuple[Union[Path, str], int, str]], transform: albu.Compose) -> None:
         """
 
         Args:
@@ -24,7 +24,7 @@ class MelanomaDataset(Dataset):
         return len(self.samples)
 
     def __getitem__(self, idx: int) -> Dict[str, Any]:
-        image_path, target = self.samples[idx]
+        image_path, target, group = self.samples[idx]
 
         image = load_rgb(image_path, lib="jpeg4py")
 
@@ -32,14 +32,15 @@ class MelanomaDataset(Dataset):
         image = self.transform(image=image)["image"]
 
         return {
-            "image_id": image_path.stem,
+            "image_id": Path(image_path).stem,
             "features": tensor_from_rgb_image(image),
-            "targets": torch.Tensor([target]),
+            "targets": torch.from_numpy(np.array([target])),
+            "group": group,
         }
 
 
 class MelanomaTestDataset(Dataset):
-    def __init__(self, samples: List[Path], transform: albu.Compose) -> None:
+    def __init__(self, samples: List[Tuple[Union[Path, str], str]], transform: albu.Compose) -> None:
         self.samples = samples
         self.transform = transform
 
@@ -47,11 +48,11 @@ class MelanomaTestDataset(Dataset):
         return len(self.samples)
 
     def __getitem__(self, idx: int) -> Dict[str, Any]:
-        image_path = self.samples[idx]
+        image_path, group = self.samples[idx]
 
         image = load_rgb(image_path, lib="jpeg4py")
 
         # apply augmentations
         image = self.transform(image=image)["image"]
 
-        return {"image_id": image_path.stem, "features": tensor_from_rgb_image(image)}
+        return {"image_id": Path(image_path).stem, "features": tensor_from_rgb_image(image), "group": group}
--- kaggle_melanoma/utils.py
+++ kaggle_melanoma/utils.py
@@ -1,14 +1,65 @@
 from pathlib import Path
-from typing import Union, Dict, List, Tuple, Optional, Any
+from typing import Union, Dict, List, Tuple, Optional, Any, Generator, DefaultDict, Counter, Set
+
+# import typing
 from sklearn import metrics
-import numpy as np
-import pandas as pd
 import re
 import torch
 
+import random
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+from collections import Counter as collections_Counter
 
-def get_samples(data_path: Path) -> List[Tuple[Path, int]]:
-    df = pd.read_csv(data_path / "train.csv")[["image_name", "target"]]
+
+def stratified_group_k_fold(target: np.ndarray, groups: np.ndarray, num_folds: int, seed: Optional[int]) -> Generator:
+    labels_num = np.max(target) + 1
+    y_counts_per_group: DefaultDict[str, np.ndarray] = defaultdict(lambda: np.zeros(labels_num))
+    y_distribution: Counter[int] = collections_Counter()
+    for t, g in zip(target, groups):
+        y_counts_per_group[g][t] += 1
+        y_distribution[t] += 1
+
+    y_counts_per_fold: DefaultDict[int, np.ndarray] = defaultdict(lambda: np.zeros(labels_num))
+    groups_per_fold: DefaultDict[int, Set[str]] = defaultdict(set)
+
+    def eval_y_counts_per_fold(y_counts: int, fold: int) -> float:
+        y_counts_per_fold[fold] += y_counts
+        std_per_label = []
+        for label in range(labels_num):
+            label_std = np.std([y_counts_per_fold[i][label] / y_distribution[label] for i in range(num_folds)])
+            std_per_label.append(label_std)
+        y_counts_per_fold[fold] -= y_counts
+        return np.mean(std_per_label)
+
+    groups_and_y_counts = list(y_counts_per_group.items())
+    random.Random(seed).shuffle(groups_and_y_counts)
+
+    for g, y_counts in sorted(groups_and_y_counts, key=lambda x: -np.std(x[1])):
+        best_fold = -1
+        min_eval = -1.0
+        for i in range(num_folds):
+            fold_eval = eval_y_counts_per_fold(y_counts, i)
+            if min_eval < 0 or fold_eval < min_eval:
+                min_eval = fold_eval
+                best_fold = i
+        y_counts_per_fold[best_fold] += y_counts
+        groups_per_fold[best_fold].add(g)
+
+    all_groups = set(groups)
+    for i in range(num_folds):
+        train_groups = all_groups - groups_per_fold[i]
+        test_groups = groups_per_fold[i]
+
+        train_indices = [i for i, g in enumerate(groups) if g in train_groups]
+        test_indices = [i for i, g in enumerate(groups) if g in test_groups]
+
+        yield train_indices, test_indices
+
+
+def get_samples(data_path: Path) -> List[Tuple[Path, int, str]]:
+    df = pd.read_csv(data_path / "train.csv")[["image_name", "target", "patient_id"]]
     df["image_name"] = df["image_name"].apply(lambda x: data_path / "jpeg" / "train" / (x + ".jpg"))
     return list(df.itertuples(index=False, name=None))
 
